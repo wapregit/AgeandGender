033dd61b414ebb681c36e27185995bee4cfe606c
tigerpaw <ronnachai.b@rmutsvmail.com>
2023-04-10 15:01:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ if photo:
     gender_model_path="gender_net.caffemodel"
 
     MODEL_MEAN_VALUES=(78.4263377603, 87.7689143744, 114.895847746)
-    age_classes=['Age: 1-2', 'Age: 3-5', 'Age: 6-14', 'Age: 16-22', 'Age: 23-30', 'Age: 31-40', 'Age: 41-50', 'Age: Greater than 50']
+    age_classes=['Age: ~1-2', 'Age: ~3-5', 'Age: ~6-14', 'Age: ~16-22', 'Age: ~23-30', 'Age: ~31-40', 'Age: ~41-50', 'Age: Greater than 50']
     gender_classes = ['Male', 'Female']
 
     age_net = cv2.dnn.readNet(age_model_path, age_txt_path) 
